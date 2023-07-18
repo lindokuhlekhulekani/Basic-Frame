25a23bafddc4a53aacbe5ddbc3433acb42a97f9d
khulekani chambale <112324289+lindokuhlekhulekani@users.noreply.github.com>
2023-07-18 13:47:58 +0200

    Add files via upload

---
+++ MyFirstFrame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author KHULEKANI
+ */
+public class MyFirstFrame extends JFrame{
+    
+    public MyFirstFrame(){
+        
+        setTitle("My Frame");
+        setSize(200, 250);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
---
+++ MyFrameApp.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myframeapp;
+
+import za.ac.tut.MyFirstFrame;
+
+/**
+ *
+ * @author KHULEKANI
+ */
+public class MyFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MyFirstFrame fr;
+        
+        fr = new MyFirstFrame();
+    }
+    
+}
